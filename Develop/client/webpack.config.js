93216f4f016facb121d7483860c90df896c6c39f
Reid Herrera <reidherrerabusiness@gmail.com>
2024-08-15 10:03:11 -0500

    tried to fix various issues

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,77 +1,63 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-    },
-    plugins: [
-      // Generates an HTML file from a template
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "J.A.T.E",
-      }),
-
-      // Injects the custom service worker
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "service-worker.js",
-      }),
-
-      // Creates a manifest.json file
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "Take notes with JavaScript syntax highlighting!",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
-        // CSS loaders
+module.exports = {
+  mode: "production", // Ensure this is set to 'production' or 'development'
+  entry: {
+    main: "./src/js/index.js",
+    install: "./src/js/install.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"), // Output to 'dist' directory
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "J.A.T.E",
+    }),
+    new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "service-worker.js",
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "Just Another Text Editor",
+      short_name: "J.A.T.E",
+      description: "Take notes with JavaScript syntax highlighting!",
+      background_color: "#225ca3",
+      theme_color: "#225ca3",
+      start_url: "./",
+      publicPath: "./",
+      icons: [
         {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        // Babel loader for ES6+
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          },
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
         },
       ],
-    },
-  };
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
